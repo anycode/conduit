f0537e21d82e8ab7f528c3a5c7f0c7beb844a6fe
Martin Edlman <ac@an.y-co.de>
2024-02-08 19:43:50 +0100

    Add extra HTTP methods, some well known, some non-standard but usable for various purposes * `PATCH` (well known) to update only passed values * `COPY` to create new record from existing * `PURGE` to delete record with all deps. * `EXEC` to exec some code on server side, no return value expected * `RESET` to reset values on server * `LOCK` to lock a record * `UNLOCK` to unlock a record
    Signed-off-by: Martin Edlman <ac@an.y-co.de>

--- packages/core/lib/src/http/resource_controller_bindings.dart
+++ packages/core/lib/src/http/resource_controller_bindings.dart
@@ -49,12 +49,34 @@ class Operation {
         _pathVariable3 = pathVariable3,
         _pathVariable4 = pathVariable4;
 
+  const Operation.patch([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "PATCH",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
   const Operation.post([
     String? pathVariable1,
     String? pathVariable2,
     String? pathVariable3,
     String? pathVariable4,
   ])  : method = "POST",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
+  const Operation.copy([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "COPY",
         _pathVariable1 = pathVariable1,
         _pathVariable2 = pathVariable2,
         _pathVariable3 = pathVariable3,
@@ -71,6 +93,61 @@ class Operation {
         _pathVariable3 = pathVariable3,
         _pathVariable4 = pathVariable4;
 
+  const Operation.purge([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "PURGE",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
+  const Operation.exec([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "EXEC",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
+  const Operation.reset([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "RESET",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
+  const Operation.lock([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "LOCK",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
+  const Operation.unlock([
+    String? pathVariable1,
+    String? pathVariable2,
+    String? pathVariable3,
+    String? pathVariable4,
+  ])  : method = "UNLOCK",
+        _pathVariable1 = pathVariable1,
+        _pathVariable2 = pathVariable2,
+        _pathVariable3 = pathVariable3,
+        _pathVariable4 = pathVariable4;
+
   final String method;
   final String? _pathVariable1;
   final String? _pathVariable2;
@@ -79,8 +156,7 @@ class Operation {
 
   /// Returns a list of all path variables required for this operation.
   List<String> get pathVariables {
-    return [_pathVariable1, _pathVariable2, _pathVariable3, _pathVariable4]
-        .fold([], (acc, s) {
+    return [_pathVariable1, _pathVariable2, _pathVariable3, _pathVariable4].fold([], (acc, s) {
       if (s != null) {
         acc.add(s);
       }
